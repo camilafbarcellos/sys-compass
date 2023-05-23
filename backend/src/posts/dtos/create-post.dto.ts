5ed1126d9171430681606ba4bf1aa140c41119ef
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2023-05-23 18:13:13 -0300

    feat: add post dtos

---
+++ backend/src/posts/dtos/create-post.dto.ts
@@ -0,0 +1,33 @@
+import {
+    IsNumber, IsString, IsArray, MinDate,
+    IsDate, MaxDate, IsNotEmpty, IsOptional
+} from 'class-validator';
+
+export class CreatePostDto {
+
+    @IsNotEmpty()
+    @IsString()
+    user: string;
+
+    @IsNotEmpty()
+    // @IsDate()
+    // @MinDate(new Date())
+    // @MaxDate(new Date())
+    post_date: Date;
+
+    @IsNotEmpty()
+    @IsString()
+    description: string;
+
+    @IsNotEmpty()
+    @IsNumber()
+    likes: number;
+
+    @IsOptional()
+    @IsArray()
+    comments?: [];
+
+    @IsOptional()
+    @IsString()
+    url_image?: string;
+}
