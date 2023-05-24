96f4fdc809384e8f18696219bb03560d75891134
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2023-05-24 17:03:41 -0300

    feat: add birthdate validation for create-user.dto

--- backend/src/users/dtos/create-user.dto.ts
+++ backend/src/users/dtos/create-user.dto.ts
@@ -1,22 +1,36 @@
-import { IsEmail, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
+import {
+  IsNotEmpty, IsOptional, IsEmail,
+  IsString, IsDate, MaxDate
+} from 'class-validator';
 
+const yesterday = new Date();
+yesterday.setDate(yesterday.getDate() - 1);
 export class CreateUserDto {
 
+  @IsNotEmpty()
   @IsString()
   name: string;
 
+  @IsNotEmpty()
   @IsString()
-  user: string;  
+  user: string;
 
-  @IsString()
-  birthdate: string;
+  @IsNotEmpty()
+  @IsDate()
+  @Transform(({ value }) => new Date(value))
+  @MaxDate(yesterday)
+  birthdate: Date;
 
+  @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @IsNotEmpty()
   @IsString()
   password: string;
 
+  @IsOptional()
   @IsString()
-  profile_photo: string;
+  profile_photo?: string;
 }
