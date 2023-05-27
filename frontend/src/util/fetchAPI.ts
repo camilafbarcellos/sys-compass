a7d7e31a6b24a87d280177268010b358991cdb07
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2023-05-27 20:04:16 -0300

    feat: add header parameter to fetch api

--- frontend/src/util/fetchAPI.ts
+++ frontend/src/util/fetchAPI.ts
@@ -1,17 +1,20 @@
-export const fetchAPI = async (endpoint: string, method: string, body?: any) => {
+export const fetchAPI = async (endpoint: string, method: string, token?: string) => {
     const myHeaders = new Headers();
     myHeaders.append('Content-Type', 'application/json');
     myHeaders.append('Accept', '*/*');
+    if(token) {
+        myHeaders.append('Authorization', 'Bearer ' + token);
+    }
 
     const myInit: RequestInit = {
         method: method,
-        headers: myHeaders,
-        body: JSON.stringify(body)
+        headers: myHeaders
     }
     const response = await fetch('http://localhost:9000/' + endpoint, myInit);
     const data = await response.json();
+    const ok = response.ok;
 
     console.log(response)
 
-    return {response: response, data: data};
+    return {response: response, data: data, ok: ok};
 }
