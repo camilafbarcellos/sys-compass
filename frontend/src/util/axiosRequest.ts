35c994bff3c15f8af2b3671f28bb964accdf2435
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2023-05-27 16:47:23 -0300

    feat: axios request returns response, data and status

--- frontend/src/util/axiosRequest.ts
+++ frontend/src/util/axiosRequest.ts
@@ -1,15 +1,14 @@
 import axios from 'axios';
-
-export async function axiosRequest (endpoint: string, method: string, data?: any) {
-    axios({
+export const axiosRequest = async (endpoint: string, method: string, body?: any) => {
+    const response = await axios({
         method: method,
-        url: 'http://localhost:9000/'+ endpoint,
-        data: data
-    })
-        .then(response => {
-            console.log(response);
-        })
-        .catch(error => {
-            console.log(error);
-        });
+        url: 'http://localhost:9000/' + endpoint,
+        data: body
+    });
+    console.log(response);
+
+    const data = await response.data;
+    const status = response.status;
+
+    return { response: response, data: data, status: status };
 }
