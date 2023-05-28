0c871f7b921dbc286d7ea9f4e62027e87f4d783e
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2023-05-28 18:02:29 -0300

    style: change body content layout

--- frontend/src/components/homepage/Body.tsx
+++ frontend/src/components/homepage/Body.tsx
@@ -1,20 +1,10 @@
 import HomePost from './HomePost';
-import Friends from './Friends';
 
 export default function Body({ localUser, posts, users, refreshPosts }: any) {
 
     return (
         <section className='homeBody'>
             <HomePost localUser={localUser} posts={posts} users={users} refreshPosts={refreshPosts} />
-
-            <section className='homeTopics'>
-                <Friends localUser={localUser} users={users} />
-
-                <section className='trendTopic' />
-
-                <section className='trendTopic' />
-
-            </section>
         </section>
     );
 }
--- frontend/src/components/homepage/Main.tsx
+++ frontend/src/components/homepage/Main.tsx
@@ -1,12 +1,24 @@
 import Body from './Body';
+import Friends from './Friends';
 import Header from './Header';
 
-export default function Main({localUser, posts, users, refreshPosts}: any) {
+export default function Main({ localUser, posts, users, refreshPosts }: any) {
 
     return (
         <section className='homeMain'>
-            <Header localUser={localUser}/>
-            <Body localUser={localUser} posts={posts} users={users} refreshPosts={refreshPosts}/>
+            <Header localUser={localUser} />
+
+            <section className='homeBodyContent'>
+                <Body localUser={localUser} posts={posts} users={users} refreshPosts={refreshPosts} />
+
+                <section className='homeTopics'>
+                    <Friends localUser={localUser} users={users} />
+
+                    <section className='trendTopic' />
+
+                    <section className='trendTopic' />
+                </section>
+            </section>
         </section>
     );
 }
