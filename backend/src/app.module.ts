c586b4019c1aa55bae9762fdb2d9bc7292a587cc
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2023-05-29 17:12:35 -0300

    build: update libs and add dotenv file and config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,21 +7,18 @@ import { User } from './users/user.entity';
 import { Comment } from './comments/comment.entity';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
     PostsModule,
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mongodb',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT) || 27017,
-      // username: process.env.DB_USERNAME,
-      // password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE || 'sys-compass',
-      // url: 'mongodb://localhost:27017',
-      // database: 'sys-compass',
-      // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      database: process.env.DB_DATABASE,
       autoLoadEntities: true,
       ssl: false,
       useUnifiedTopology: true,
